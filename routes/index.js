663d760a6b240fc247c90bb37e582e1c289cafea
Prabhakar Acharya <acharyaP03@gmail.com>
2020-07-10 16:40:31 +1000

    route to show all blogs of same user

--- routes/index.js
+++ routes/index.js
@@ -9,47 +9,7 @@ const Blogs = require('./../Models/Blogs');
 
 //@route GET 
 
-router.get('/blog', async (req, res) =>{
-    //get all the public blogs and display it to the main blog page. 
-    console.log('Inside main blog page')
-    console.log(req.params.id)
-    try {
-        const blogs = await Blogs.find({ status : 'public'}).populate('user').sort({ createdAt: 'desc'}).lean();
-        const blog = await Blogs.findById(req.params.id).lean();
-        //here we need to specify the render function to use login layout no the main. 
-        // if we dont specify it then it will use main as a default since we have main as a default on app js. 
-        console.log(blogs)
-        console.log(blog)
-        res.render('blogpages/main', {
-            blogs, 
-            layout: 'mainblog'
-        })
-    } catch (err) {
-        console.log(err);
-        res.render('error/500');
-    }
-})
 
-//@desc Single blog page 
-
-//@route GET 
-
-router.get('/blog/:id', async (req, res) =>{
-    //get all the public blogs and display it to the main blog page. 
-    console.log('I here')
-    try {
-        const blogs = await Blogs.findById(req.params.id).lean();
-        //here we need to specify the render function to use login layout no the main. 
-        // if we dont specify it then it will use main as a default since we have main as a default on app js. 
-        res.render('blogpages/singleblog', {
-            blogs, 
-            layout: 'mainblog'
-        })
-    } catch (err) {
-        console.log(err);
-        res.render('error/500');
-    }
-})
 
 //@desc Login/Landing page
 
